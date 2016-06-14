4a534bae50f1cbc02ceed5928c3726764353e7d9
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-14 10:08:41 -0700

    add the same attributes we previously used in our hard-coded array of JavaScript objects: Owner, city, type, image, and bedrooms:

--- app/models/rental.js
+++ app/models/rental.js
@@ -3,5 +3,13 @@ import DS from 'ember-data';
 // import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default DS.Model.extend({
+    owner: DS.attr(),
+    city: DS.attr(),
+    type: DS.attr(),
+    image: DS.attr(),
+    bedrooms: DS.attr()
+});
+
+export default DS.Model.extend({
 
 });
