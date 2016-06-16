c55987015ea269c6af89fa58d3c6e7b437060d73
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-16 14:41:19 -0700

    add notes on using params.rental_id in the model hook

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 
+//Notice that we're using the method findRecord() instead of findAll() here. Additionally, findRecord() takes two arguments: The type of object ('rental'), and the object's specific id (params.rental_id). After retrieving the specified rental, the route will render the rental template.
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('rental', params.rental_id);
