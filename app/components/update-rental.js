9cf3f521a310b7e1c7ec089b428f4ce05d2242e4
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-16 13:25:05 -0700

    add notes on component var names assigned to values needing to matvh model attribute names

--- app/components/update-rental.js
+++ app/components/update-rental.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
+//t's important that the variable names we assign these values match the rental model’s attribute names (ie: owner, city, type, image, and bedrooms; the same attributes we defined on our model in models/rental.js). If they do not, we will receive errors when assigning these new values to our rental object.
 export default Ember.Component.extend({
+  updateRentalForm: false,
+  actions: {
+    updateRentalForm() {
+      this.set('updateRentalForm', true);
+    },
+    update(rental) {
+      var params = {
+        owner: this.get('owner'),
+        city: this.get('city'),
+        type: this.get('type'),
+        image: this.get('image'),
+        bedrooms: this.get('bedrooms'),
+      };
+      this.set('updateRentalForm', false);
+      this.sendAction('update', rental, params);
+    }
+  }
 });
