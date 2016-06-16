eea53dca58ceeef5e3df3a36b9e0e4cfa5f2a2cc
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-16 14:30:09 -0700

    add notes on how dynamic segment works

--- app/router.js
+++ app/router.js
@@ -5,6 +5,7 @@ const Router = Ember.Router.extend({
   location: config.locationType
 });
 
+//Now, when the router receives a request matching /rental/:rental_id (such as from a link within our application that contains the rental's id) it will map the URL to the rental route handler, rental.js, and send a params hash that includes the value of the :rental_id segment in the URL.
 Router.map(function() {
   this.route('about');
   this.route('contact');
