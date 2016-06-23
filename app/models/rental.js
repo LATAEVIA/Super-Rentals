bd3e26df8beda87c531743e01789030e5fa08fe4
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-22 17:30:20 -0700

    update notes on ds attribute, add a model called city and define its attributes

--- app/models/rental.js
+++ app/models/rental.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 // import attr from 'ember-data/attr';
 // import { belongsTo, hasMany } from 'ember-data/relationships';
 
-//This is a list of object attributes we want to save to our store for each rental we add. When a DS.Model is exported, as we see in the line export default DS.Model.extend({ ... it is added to the data store. That is, it's being saved to our database.
+// The DS in this file stands for "data store". .attr() stands for 'attribute'. This is a list of object attributes we want to save to our store for each rental we add. When a DS.Model is exported, as we see in the line export default DS.Model.extend({ ... it is added to the data store. That is, it's being saved to our database.
 export default DS.Model.extend({
     owner: DS.attr(),
     city: DS.attr(),
