87aaba341070d821dad475593e2702e7191dc98a
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-23 00:02:53 -0700

    add the city id to route as a dynamic segment, and remove comment re: dynamic segments

--- app/router.js
+++ app/router.js
@@ -5,12 +5,11 @@ const Router = Ember.Router.extend({
   location: config.locationType
 });
 
-//Now, when the router receives a request matching /rental/:rental_id (such as from a link within our application that contains the rental's id) it will map the URL to the rental route handler, rental.js, and send a params hash that includes the value of the :rental_id segment in the URL.
 Router.map(function() {
   this.route('about');
   this.route('contact');
   this.route('rental', {path: '/rental/:rental_id'});
-  this.route('city');
+  this.route('city', {path: '/city/:city_id'});
 });
 
 export default Router;
