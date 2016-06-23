e0bcccb3c33d242c22106d6ecd7be58a4ebb05eb
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-22 23:30:05 -0700

    change the city attribute on the rental model to refer to one of our city objects, instead of just containing a string

--- app/models/rental.js
+++ app/models/rental.js
@@ -5,7 +5,7 @@ import DS from 'ember-data';
 // The DS in this file stands for "data store". .attr() stands for 'attribute'. This is a list of object attributes we want to save to our store for each rental we add. When a DS.Model is exported, as we see in the line export default DS.Model.extend({ ... it is added to the data store. That is, it's being saved to our database.
 export default DS.Model.extend({
     owner: DS.attr(),
-    city: DS.attr(),
+    city: DS.belongsTo('city', { async: true }),
     type: DS.attr(),
     image: DS.attr(),
     bedrooms: DS.attr()
