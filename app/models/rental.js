5136b59b56ddca57e13dc30df2bc3170a747c29f
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-22 23:34:34 -0700

    add notes on { async: true } and change referenced to rental.city because that would now point to an object since we established a one to many relationship

--- app/models/rental.js
+++ app/models/rental.js
@@ -3,6 +3,8 @@ import DS from 'ember-data';
 // import { belongsTo, hasMany } from 'ember-data/relationships';
 
 // The DS in this file stands for "data store". .attr() stands for 'attribute'. This is a list of object attributes we want to save to our store for each rental we add. When a DS.Model is exported, as we see in the line export default DS.Model.extend({ ... it is added to the data store. That is, it's being saved to our database.
+
+//The { async: true } property means that if the related model data isn't already loaded, an additional request should be made to Firebase in order to retrieve it.
 export default DS.Model.extend({
     owner: DS.attr(),
     city: DS.belongsTo('city', { async: true }),
