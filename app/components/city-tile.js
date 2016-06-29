ad0f5a7418f49f8f7fc88c7b9978c748988b73a6
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-29 12:40:31 -0700

    correct misspelling in js and update city-tile has to make use of new computed property

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  fullLocation : Ember.computed('city.anme', 'city.country', function(){
+  fullLocation : Ember.computed('city.name', 'city.country', function(){
     return this.get('city.name') + ', ' + this.get('city.country');
   }),
   actions: {
