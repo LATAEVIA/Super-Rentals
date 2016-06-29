a560e89121eecb26c1f7019e526f366fd65c3a2e
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-29 12:42:51 -0700

    note how ember computed pulls property data

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -4,6 +4,7 @@ export default Ember.Component.extend({
   fullLocation : Ember.computed('city.name', 'city.country', function(){
     return this.get('city.name') + ', ' + this.get('city.country');
   }),
+  // First two ember computed parameters are to be observed and to auto update function when they change
   actions: {
     destroyCity(city) {
       if (confirm('Are you sure you want to delete this city?')) {
